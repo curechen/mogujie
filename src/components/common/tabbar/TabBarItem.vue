494b0d3811866a9ddb9c6b37e7430a87dac2ee4c
curechen <981470148@qq.com>
2021-07-21 08:52:32 +0800

    这是第一次提交

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tab-bar-item">
+    <div class="icon"><slot name="item-icon"></slot></div>
+    <slot name="item-text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+};
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+  
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
